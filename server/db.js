49a1f241c798ec6b325f0a75fcd262cdbeacdc58
Luis Villalobos <luisnazario2310@gmail.com>
2020-12-26 16:09:27 -0400

    Prueba

--- server/db.js
+++ server/db.js
@@ -1,12 +1,26 @@
 const Pool = require('pg').Pool
 
-const pool = new Pool({
-    user: "postgres",
-    password: "12345",
-    host: "localhost",
-    port: 5432,
-    database: "PruebaTesis"
-})  
+let pool
+
+if (process.env.NODE_ENV === 'production') {
+
+    pool = new Pool({
+        user: "postgres",
+        password: "12345",
+        host: "localhost",
+        port: 5432,
+        database: "PruebaTesis"
+    })  
+
+} else {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      });
+}
+
 
 
 pool.connect((err, client, done) => {
