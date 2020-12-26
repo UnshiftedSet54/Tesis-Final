317420ca45ed1a69ae51a973636961d62a6395ff
Luis Villalobos <luisnazario2310@gmail.com>
2020-12-26 19:21:51 -0400

    Nuevo x16

--- server/routes/userinfo.js
+++ server/routes/userinfo.js
@@ -7,31 +7,38 @@ router.get('/userinfo/:id', async (req, res) => {
 
     const { id } = req.params
 
-   const resp = await pool.query("select * from freelancerusuario inner join free_area on freelancerusuario.username_freelancer = free_area.username_freelancer inner join area on area.area_id = free_area.area_id where freelancerusuario.username_freelancer = $1", [id])
+    try {
 
-   let result = 
-    {
-        username_freelancer: resp.rows[0].username_freelancer,
-        username_nombre: resp.rows[0].nombre_completo,
-        estado: resp.rows[0].estado,
-        pdf_url: resp.rows[0].pdf_url,
-        rubro_id: resp.rows[0].rubro_id,
-        area_info: resp.rows.map(v => {
-            return {
-                nombre_area: v.nombre,
-                experiencia: v.experiencia
-            }
-        })
+        const resp = await pool.query("select * from freelancerusuario inner join free_area on freelancerusuario.username_freelancer = free_area.username_freelancer inner join area on area.area_id = free_area.area_id where freelancerusuario.username_freelancer = $1", [id])
+     
+        let result = 
+         {
+             username_freelancer: resp.rows[0].username_freelancer,
+             username_nombre: resp.rows[0].nombre_completo,
+             estado: resp.rows[0].estado,
+             pdf_url: resp.rows[0].pdf_url,
+             rubro_id: resp.rows[0].rubro_id,
+             area_info: resp.rows.map(v => {
+                 return {
+                     nombre_area: v.nombre,
+                     experiencia: v.experiencia
+                 }
+             })
+         }
+     
+        return res.status(200).json({ result })
+
+
+    } catch (err) {
+        console.log(err)
     }
 
-   return res.status(200).json({ result })
 
 })
 
 router.get('/getuserinfologged', async (req, res) => {
 
-    console.log("REQ USER", req.user)
-
+    
 
     if (req.user.isbussines) {
 
@@ -39,44 +46,50 @@ router.get('/getuserinfologged', async (req, res) => {
 
     } else {
 
-        const resp = await pool.query("select * from freelancerusuario inner join free_area on freelancerusuario.username_freelancer = free_area.username_freelancer inner join rubro on rubro.rubro_id = free_area.rubro_id  inner join area on area.area_id = free_area.area_id where freelancerusuario.username_freelancer = $1", [req.user.username_freelancer])
+        try {
+            const resp = await pool.query("select * from freelancerusuario inner join free_area on freelancerusuario.username_freelancer = free_area.username_freelancer inner join rubro on rubro.rubro_id = free_area.rubro_id  inner join area on area.area_id = free_area.area_id where freelancerusuario.username_freelancer = $1", [req.user.username_freelancer])
+    
+            const propuestas = await pool.query("select * from propuesta where user_prop = $1", [req.user.username_freelancer])
+    
+            const rubro = await pool.query("select * from rubro")
+    
+            const checkPropuestasAreaName = await pool.query('select * from propuesta inner join anuncios_area on anuncios_area.anuncio_id = propuesta.anuncio_id inner join area on anuncios_area.area_id = area.area_id where user_prop = $1', [req.user.username_freelancer])
+    
+            const propuestasReadNumber = propuestas.rows.filter(v =>  v.isread === true ).length
+    
+            const filterRepeated = checkPropuestasAreaName.rows.filter((valor, i) => {
+                return checkPropuestasAreaName.rows.findIndex(value => value.area_id === valor.area_id ) === i
+            })
+    
+            const finalData = filterRepeated.map(valor => {
+                return {
+                    nombre: valor.nombre,
+                    veces: checkPropuestasAreaName.rows.filter(value => value.area_id === valor.area_id  ).length
+                }
+            })
+    
+    
+       let result = 
+        {
+            username_freelancer: resp.rows[0].username_freelancer,
+            username_nombre: resp.rows[0].nombre_completo,
+            estado: resp.rows[0].estado,
+            pdf_url: resp.rows[0].pdf_url,
+            rubro: rubro.rows.find(v => v.rubro_id === resp.rows[0].rubro_id ).nombre,
+            area_info: resp.rows.map(v => {
+                return {
+                    nombre_area: v.nombre,
+                    experiencia: v.experiencia
+                }
+            })
+        }
+    
+        return res.status(200).json({ result, propuestasNumber: propuestas.rows.length, propuestasReadNumber: propuestasReadNumber, finalData  })
 
-        const propuestas = await pool.query("select * from propuesta where user_prop = $1", [req.user.username_freelancer])
+        } catch (err) {
+            console.log(err)
+        }
 
-        const rubro = await pool.query("select * from rubro")
-
-        const checkPropuestasAreaName = await pool.query('select * from propuesta inner join anuncios_area on anuncios_area.anuncio_id = propuesta.anuncio_id inner join area on anuncios_area.area_id = area.area_id where user_prop = $1', [req.user.username_freelancer])
-
-        const propuestasReadNumber = propuestas.rows.filter(v =>  v.isread === true ).length
-
-        const filterRepeated = checkPropuestasAreaName.rows.filter((valor, i) => {
-            return checkPropuestasAreaName.rows.findIndex(value => value.area_id === valor.area_id ) === i
-        })
-
-        const finalData = filterRepeated.map(valor => {
-            return {
-                nombre: valor.nombre,
-                veces: checkPropuestasAreaName.rows.filter(value => value.area_id === valor.area_id  ).length
-            }
-        })
-
-
-   let result = 
-    {
-        username_freelancer: resp.rows[0].username_freelancer,
-        username_nombre: resp.rows[0].nombre_completo,
-        estado: resp.rows[0].estado,
-        pdf_url: resp.rows[0].pdf_url,
-        rubro: rubro.rows.find(v => v.rubro_id === resp.rows[0].rubro_id ).nombre,
-        area_info: resp.rows.map(v => {
-            return {
-                nombre_area: v.nombre,
-                experiencia: v.experiencia
-            }
-        })
-    }
-
-    return res.status(200).json({ result, propuestasNumber: propuestas.rows.length, propuestasReadNumber: propuestasReadNumber, finalData  })
 
     }
 })
